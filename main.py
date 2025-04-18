15e3aacb7f3cc89743ae0cfd49076709b81a03eb
Nemo Tamang <nemot54@gmail.com>
2025-04-18 21:35:46 +0545

    accuracy 5/20

--- main.py
+++ main.py
@@ -4,23 +4,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
-mnist = tf.keras.daasets.mnist
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-x_train = tf.keras.utils.normalize(x_train, axis=1)
-x_test = tf.keras.utils.normalize(x_test, axis=1)
+model = tf.keras.models.load_model('handwritten_model.keras')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+image_number = 1
+while os.path.isfile(f"digits/digit{image_number}.png"):
+    try:
+        img = cv2.imread(f"digits/digit{image_number}.png")[:, :, 0]
+        img = np.invert(np.array([img]))
+        prediction = model.predict(img)
+        print(f"The number is probably: {np.argmax(prediction)}")
+        plt.imshow(img[0], cmap=plt.cm.binary)
+        plt.show()
+        
+    except:
+        print(f"Error processing the image digit{image_number}.png")
+        
+    finally:
+        image_number += 1
--- model_creation.py
+++ model_creation.py
@@ -19,6 +19,6 @@ model.add(tf.keras.layers.Dense(10, activation='softmax'))
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=3)
+model.fit(x_train, y_train, epochs=11)
 
 model.save('handwritten_model.keras')
